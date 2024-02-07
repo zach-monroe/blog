e0ad1ea4be490a528e90065bc80367dcd6853358
Zach Monroe <z@localhost.localdomain>
2024-02-07 10:12:28 -0500

    First Draft Complete
    Site has met all requirements.
    - Users can contribute posts
    - Users can edit posts
    - Users can delete posts
    - All posts are dispalyed on the home page.
    
    Areas to improve for future projects:
    - There was some code spaghetti to make this work, need to make this cleaner.
    - Need to implement individual users and data validation (outside of the scope of this project, but would have made put and delete requests much less hackish.)

--- index.js
+++ index.js
@@ -19,13 +19,39 @@ app.post("/submit", (req, res) => {
   var title = req.body["title"];
   var post = req.body["post"];
   posts.push({ author: author, title: title, post: post });
+  res.redirect("/");
+});
+app.post("/edit", (req, res) => {
+  var id = req.body["id"];
+  res.render("write.ejs", { id: id, posts: posts });
+});
+
+app.post("/edit/submit", (req, res) => {
+  var id = req.body["id"];
+  var author = req.body["author"];
+  var title = req.body["title"];
+  var post = req.body["post"];
+  posts[id] = { author: author, title: title, post: post };
+  res.redirect("/");
+});
+app.post("/delete", (req, res) => {
+  var id = req.body["id"];
+  posts.splice(id, 1);
+  res.render("index.ejs", { posts: posts });
 });
 app.listen(port, () => {
   console.log(`Server is live at port ${port} .`);
 });
 
-var posts = [];
-
-// TODO: Make Example Posts
-// TODO: Add Edit Functionality https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/forms
-// TODO: Add Delete Functionality
+var posts = [
+  {
+    author: "C.S. Lewis",
+    title: "Great Quote",
+    post: "Courage is not simply one of the virtues, but the form of every virtue at the testing point.",
+  },
+  {
+    author: "Aristotle",
+    title: "Another Great Quote!",
+    post: "It is the mark of an educated mind to be able to entertain a thought without accepting it.",
+  },
+];
